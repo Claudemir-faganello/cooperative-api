cd58d58c65b983a9271c13cb44087f66d2961495
claudemir.faganello <faganello.claudemir@gmail.com>
2023-08-31 14:38:10 -0300

    Initial project setup

---
+++ src/main/java/br/com/cooperativeapi/CooperativeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cooperativeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cooperativeapi/CooperativeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cooperativeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
