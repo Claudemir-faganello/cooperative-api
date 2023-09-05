d50c3a0bdf18114ed7cd95c57f797a6387b217be
claudemir.faganello <faganello.claudemir@gmail.com>
2023-09-04 22:53:20 -0300

    Modifica classe principal

--- src/main/java/br/com/cooperativeapi/CooperativeApiApplication.java
+++ src/main/java/br/com/cooperativeapi/CooperativeApiApplication.java
@@ -1,13 +1,21 @@
 package br.com.cooperativeapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.cooperativeapi")
+@EntityScan("br.com.cooperativeapi.entity")
+@EnableJpaRepositories("br.com.cooperativeapi.repository")
+@EnableAutoConfiguration
 public class CooperativeApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CooperativeApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CooperativeApiApplication.class, args);
+    }
 
 }
