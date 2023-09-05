6c28a46608c8c01e123e89c31c7b452ff663d137
claudemir.faganello <faganello.claudemir@gmail.com>
2023-09-04 22:52:47 -0300

    Cria  AssociatedStatus

---
+++ src/main/java/br/com/cooperativeapi/client/response/AssociatedStatus.java
@@ -0,0 +1,19 @@
+package br.com.cooperativeapi.client.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssociatedStatus {
+    private static final String ABLE_TO_VOTE = "ABLE_TO_VOTE";
+
+    private String status;
+
+    public Boolean isAbleToVote() {
+        return getStatus().equalsIgnoreCase(ABLE_TO_VOTE);
+    }
+}
